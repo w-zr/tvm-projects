c65f28d350e1aea9bd0e4a536edfdd62e02c3ff4
Ziran <weiziran125@gmail.com>
2020-04-05 11:17:24 +0800

    改为time.time()

--- ssd/ssd.py
+++ ssd/ssd.py
@@ -12,7 +12,7 @@ image_path = '9331584514251_.pic_hd.jpg'
 image = cv2.imread(image_path)
 img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-pre_start = time.process_time()
+pre_start = time.time()
 
 resize_shape = (300, 300)
 img = cv2.resize(img, resize_shape, interpolation=cv2.INTER_LINEAR)
@@ -22,7 +22,7 @@ img = img.astype(np.float32)
 img = cv2.subtract(img, mean)
 img = cv2.multiply(img, std)
 img = img.transpose(2, 0, 1)
-pre_end = time.process_time()
+pre_end = time.time()
 
 # load onnx model and build tvm runtime
 target = 'llvm'
@@ -33,19 +33,20 @@ input_blob = mssd.graph.input[0]
 input_shape = tuple(map(lambda x: getattr(x, 'dim_value'), input_blob.type.tensor_type.shape.dim))
 shape_dict = {input_blob.name: input_shape}
 mod, params = relay.frontend.from_onnx(mssd, shape_dict)
+
 with relay.build_config(opt_level=3):
     graph, lib, params = relay.build(mod, target, params=params)
 
 ######## export lib ########
-# path = 'model/'
-# path_lib = path+"deploy_lib.tar"
-# path_graph = path+"deploy_graph.json"
-# path_params = path+"deploy_param.params"
-# lib.export_library(path_lib)
-# with open(path_graph, "w") as fo:
-#     fo.write(graph)
-# with open(path_params, "wb") as fo:
-#     fo.write(relay.save_param_dict(params))
+#path = 'model_tuned/'
+#path_lib = path+"deploy_lib.tar"
+#path_graph = path+"deploy_graph.json"
+#path_params = path+"deploy_param.params"
+#lib.export_library(path_lib)
+#with open(path_graph, "w") as fo:
+#    fo.write(graph)
+#with open(path_params, "wb") as fo:
+#    fo.write(relay.save_param_dict(params))
 
 ######## load lib ########
 # load the module back.
@@ -56,16 +57,13 @@ with relay.build_config(opt_level=3):
 module = runtime.create(graph, lib, ctx)
 
 # run
-# module.load_params(bytearray(params))
+# module.load_params(params)
 module.set_input(**params)
 module.set_input('input.1', tvm.nd.array(img))
-time_list = []
-post_time_list = []
 
-start = time.process_time()
+start = time.time()
 module.run()
-end = time.process_time()
-time_list.append(end - start)
+end = time.time()
 
 # generate anchor
 from anchor import gen_anchors
@@ -87,21 +85,14 @@ cfg = dict(
 cfg = EasyDict(cfg)
 
 # get output
-post_start = time.process_time()
+post_start = time.time()
 cls_score_list = [module.get_output(i).asnumpy()[0] for i in range(6)]
 bbox_pred_list = [module.get_output(i + 6).asnumpy()[0] for i in range(6)]
 
 # recover bbox
 proposals = get_bboxes_single(cls_score_list, bbox_pred_list, mlvl_anchors, resize_shape, scale_factor, cfg,
                               rescale=True)
-post_end = time.process_time()
-post_time_list.append(post_end - post_start)
-
-from heapq import nlargest
-
-print(sum(nlargest(20, time_list)) / 20)
-print(sum(nlargest(20, post_time_list)) / 20)
-
+post_end = time.time()
 
 from vis_bbox import imshow_det_bboxes
 
